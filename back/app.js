10fcd80dad76f172e03084f8b98c096313c97d46
taovc <tao.weijie@epitech.eu>
2022-08-25 18:54:47 +0200

    [add] server.js for main router

--- back/app.js
+++ back/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
 
-app.set('port', 3000);
+app.set("port", 3000);
 
 app.use(express.json());
 
 getdata = (req, res, next) => {
-    console.log(req.body.name)
-    res.status(200).json({message:"yes"})
-}
+  console.log(req.body.name);
+  res.status(200).json({ message: "yes" });
+};
 
-app.post('/api/datas', getdata)
+app.post("/api/datas", getdata);
 
-app.listen(4000)
+module.exports = app;
