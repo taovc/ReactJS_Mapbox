2c1f6c657896940b97144865f26fdd45cd3e4dd8
taovc <tao.weijie@epitech.eu>
2022-08-25 16:10:06 +0200

    [add] user data to test all function that show user infomation

--- src/ListDatas.jsx
+++ src/ListDatas.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import AddForm from "./AddForm.jsx";
 
 const AddButton = ({ setOpen }) => {
   return (
@@ -8,8 +9,148 @@ const AddButton = ({ setOpen }) => {
   );
 };
 
+const ListUsers = ({ setShowInfo, showInfo, setLat, setLng }) => {
+  const [userInfo, setUserInfo] = useState({});
+  const users = [
+    {
+      id: 1,
+      name: "Jay",
+      description: "Bilzerian",
+      type: "entreprise",
+      lng: 0,
+      lat: 1,
+    },
+    {
+      id: 2,
+      name: "Jay2",
+      description: "Bilzerian2",
+      type: "entreprise2",
+      lng: 0,
+      lat: 1,
+    },
+    {
+      id: 3,
+      name: "Jay3",
+      description: "Bilzerian3",
+      type: "entreprise3",
+      lng: 0,
+      lat: 1,
+    },
+    {
+      id: 4,
+      name: "Jay4",
+      description: "Bilzerian4",
+      type: "entreprise4",
+      lng: 0,
+      lat: 1,
+    },
+    {
+      id: 5,
+      name: "Jay5",
+      description: "Bilzerian5",
+      type: "entreprise5",
+      lng: 0,
+      lat: 1,
+    },
+    {
+      id: 6,
+      name: "Jay6",
+      description: "Bilzerian6",
+      type: "entreprise6",
+      lng: 0,
+      lat: 1,
+    },
+  ];
+
+  const handleClick = (user) => {
+    setShowInfo(!showInfo);
+    setUserInfo(user);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        height: "100vh",
+        margin: "40px",
+      }}
+    >
+      {users.map((user, index) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              justifyContent: "center",
+              width: "200px",
+              margin: "20px",
+              backgroundColor: "lightblue",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              handleClick(user);
+            }}
+            key={index}
+          >
+            <div key={index}>{user.name}</div>
+            {showInfo && (
+              <AddForm
+                setOpen={setShowInfo}
+                lat={userInfo.lat}
+                lng={userInfo.lng}
+                setLat={setLat}
+                setLng={setLng}
+                DefaultName={userInfo.name}
+                DefaultDescription={userInfo.description}
+                DefaultType={userInfo.type}
+              />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const ListUsers1 = () => {
+  const [users, setUsers] = useState([
+    { id: 1, Name: "Jay", Description: "Bilzerian", Type: "entreprise" },
+    { id: 2, Name: "Jay2", Description: "Bilzerian2", Type: "entreprise2" },
+  ]);
+
+  return (
+    <div className="containerUser">
+      <h3 className="p-3 text-center">Display a list of users</h3>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users &&
+            users.map((user) => (
+              <tr key={user.id}>
+                <th>{user.Name}</th>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 const ListDatas = ({ setOpen }) => {
   return <AddButton setOpen={setOpen} />;
 };
 
-export default ListDatas;
+const UserAction = {
+  ListUsers,
+  ListDatas,
+};
+
+export default UserAction;
