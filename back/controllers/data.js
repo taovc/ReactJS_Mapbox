22dfcb7653bcbb21b5b1c29dea64d39cb5de9bfa
taovc <tao.weijie@epitech.eu>
2022-08-26 11:14:22 +0200

    [add] router for create user data

---
+++ back/controllers/data.js
@@ -0,0 +1,27 @@
+//module.exports = (req, res, next) => {
+//  console.log(req.body.name);
+//  res.status(200).json({ message: "yes" });
+//};
+const Thing = require("../models/userdata").UserData;
+
+exports.createThing = (req, res, next) => {
+  console.log(req.body);
+  const thingObject = req.body;
+  const thing = new Thing({
+    ...thingObject,
+  });
+  //thing
+  //  .save()
+  //  .then(() => {
+      Thing.find()
+        .then((things) => {
+          res.status(200).json(things);
+        })
+        .catch((error) => {
+          res.status(400).json({ error: error });
+        });
+    //})
+    //.catch((error) => {
+    //  res.status(400).json({ error });
+    //});
+};
