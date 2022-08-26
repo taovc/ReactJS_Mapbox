d8eafbb94c25740b4f64e98df1c0f54bcdebb429
taovc <tao.weijie@epitech.eu>
2022-08-26 11:49:09 +0200

    [add] getalldata controller

--- back/routes/data.js
+++ back/routes/data.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const dataCtrl = require('../controllers/data')
 
 router.post('/datas', dataCtrl.createThing)
+router.get('/', dataCtrl.getAllData)
 
 module.exports = router
